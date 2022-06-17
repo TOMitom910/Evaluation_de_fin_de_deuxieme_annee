1d0e3f7a3d363719faf52fce6f8a0deb807c8a4e
TOMitom910 <t.lejeune@Ludus-academie.com>
2022-06-17 15:49:58 +0200

    verification et modification des dernier fichiers avant l'envoi

---
+++ BDD_Web/DB_MySQL_gamers_insert data.sql
@@ -0,0 +1,22 @@
+INSERT INTO joueur(Pseudo,Genre) VALUES
+("TOMitom910","masculin"),
+("Viseral","masculin"),
+("Skillou","masculin"),
+("Jambon_beurre","feminin"),
+("xx_Pierre_Jacques","non-binaire");
+
+INSERT INTO jeux(Nom_Jeu,Version_Jeu) VALUES
+("Rocket League","2.16"),
+("Rocket League","2.15"),
+("League of Legends","12.11"),
+("Dofus","3.6.14"),
+("Subnautica","5.6"),
+("Ori and the blind forest","1.05");
+
+INSERT INTO score(Score,Date_Score,ID_Joueur,ID_Jeux) VALUES
+("5 buts","2020-03-24",1,1),
+("800 pts","2022-05-30",1,2),
+("15 KDA","2015-08-25",3,3),
+("1 milliard de kama","2012-04-18",5,4),
+("12 minutes","2021-09-12",2,6),
+("5 defaites d'affilé", "2020-07-15",3,1);
